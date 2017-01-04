2e2acad6cc1480f627aa926e4f6658239421c8a6
TudorTacal <tudortacal@gmail.com>
2017-01-04 09:19:47 +0000

    First commit

--- tests/noteListViewSpec.js
+++ tests/noteListViewSpec.js
@@ -15,8 +15,10 @@ function returnsListModelHtml () {
   view.list.notes[0] = new Note("I like beer");
   view.list.notes[1] = new Note("I like pizza");
 
+
   if(htmlList(view.list) !== "<ul><li><div>I like beer</div></li><li><div>I like pizza</div></li></ul>") {
     throw new Error("Returned incorrect string");
+
   }
   else {
     console.log("returnsListModelHtml  passed");
